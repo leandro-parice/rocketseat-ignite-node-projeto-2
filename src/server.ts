e018af5e8507700ea6c85577b0f498041cba31a1
Leandro Parice <contato@leandroparice.com.br>
2023-05-29 22:51:28 -0300

    criado a tabela transactions

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*')
-  return test
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
 })
 
 app
