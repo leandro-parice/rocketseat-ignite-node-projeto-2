179f53c36df551bb9c5a9a74fbb39ef86520735a
Leandro Parice <contato@leandroparice.com.br>
2023-05-30 22:49:01 -0300

    validacao de variaveis de ambiente com zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { knex } from './database'
 import { randomUUID } from 'crypto'
+import { env } from './env'
 
 const app = fastify()
 
@@ -27,7 +28,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running')
