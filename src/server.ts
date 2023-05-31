9c53b3c7d1885f43ff5c6ae5b31fc26aaab9b44c
Leandro Parice <contato@leandroparice.com.br>
2023-05-30 22:34:39 -0300

    variaveis e ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  // const tables = await knex('sqlite_schema').select('*')
+  // return tables
+
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: randomUUID(),
+  //     title: 'Transação',
+  //     amount: 5000,
+  //   })
+  //   .returning('*')
+
+  // return transaction
+
+  const transaction = await knex('transactions').select('*').where({
+    amount: 1000,
+  })
+
+  return transaction
 })
 
 app
