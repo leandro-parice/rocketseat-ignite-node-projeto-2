e4122556ba862da852538a22fc86b0de01a5d8d7
Leandro Parice <contato@leandroparice.com.br>
2023-05-31 23:10:07 -0300

    criado rotas de insercao, listagem e visualizacao de transicao

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import { randomUUID } from 'crypto'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // const tables = await knex('sqlite_schema').select('*')
-  // return tables
-
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: randomUUID(),
-  //     title: 'Transação',
-  //     amount: 5000,
-  //   })
-  //   .returning('*')
-
-  // return transaction
-
-  const transaction = await knex('transactions').select('*').where({
-    amount: 1000,
-  })
-
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
